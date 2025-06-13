2319e6e08ea108801ea48063257e1694a632594f
tomrss <tommaso.rossi@pagopa.it>
2025-06-13 11:25:01 +0200

    feat(azure_github_federated_env): Add env output

--- azure_github_federated_env/outputs.tf
+++ azure_github_federated_env/outputs.tf
@@ -3,3 +3,8 @@ output "principal_id" {
   description = "Principal id of the managed identity federated with GitHub environment"
   value       = azurerm_user_assigned_identity.this.principal_id
 }
+
+output "environment" {
+  description = "GitHub environment"
+  value       = github_repository_environment.this.environment
+}
